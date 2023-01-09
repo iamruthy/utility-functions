cb5e953e1e54512f9472991f91632eb7d890e64b
rutham <ruth@datyra.com>
2023-01-09 10:45:24 -0600

    Addes getRandomNumber function

--- getRandomNumber.js
+++ getRandomNumber.js
@@ -1,3 +1,3 @@
-function getRandomElement(arr){
-    return arr[Math.floor(Math.random()*arr.length)]
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
